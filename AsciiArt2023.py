2613a4049b1742f766cd506b49ac174e302984c6
CoffeeCoding <msdevacc@live.de>
2023-06-22 01:41:07 +0200

    Compress and allow for better config

--- AsciiArt2023.py
+++ AsciiArt2023.py
@@ -4,12 +4,11 @@ import sys
 import math
 import numpy as np
 
+# CONFIG 
 brightness = ' .-=;?#%@'
-
-# CONFIG (DIMENSIONS)
-WIDTH_OUT = 32           
-
-filename = 'img.png'
+WIDTH_OUT = 40          
+ADD_SPACES = True
+filename = 'cup.jpg'
 
 # Load and convert to BW
 image = Image.open(filename)
@@ -19,6 +18,9 @@ image = image.convert("L")
 width, height = image.size
 if width > WIDTH_OUT :
     HEIGHT_OUT = int(round((WIDTH_OUT / width) * height))
+    # Vertically compress if we don't add spaces to keep dimensions acceptable
+    if ADD_SPACES :
+        HEIGHT_OUT = round(0.67 * HEIGHT_OUT)
     image = image.resize((WIDTH_OUT, HEIGHT_OUT), Image.BICUBIC)
 
 # Convert to list
@@ -31,7 +33,7 @@ fn_normalize = lambda b: (b - b_min) / b_span
 image_norm = [(b - b_min) / b_span for b in image]
 
 # Convert to ASCII
-image_ascii = [(brightness[math.floor(x * (len(brightness)-1))] + ' ') for x in image_norm]
+image_ascii = [(brightness[math.floor(x * (len(brightness)-1))] + (' ' if ADD_SPACES else '')) for x in image_norm]
 
 # Split lines
 ascii_out = ''
@@ -45,3 +47,7 @@ while i < len(image_ascii) :
 # Print result
 print(ascii_out)
 
+outfile = open(filename.split('.')[0] + ".txt", "w")
+outfile.write(ascii_out)
+outfile.flush()
+outfile.close()
